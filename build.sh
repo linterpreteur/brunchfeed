cdbbc2596b15f4b536e284665a2c1b8d9057c46b
linterpreteur <wooseop.kim.dev@gmail.com>
2017-06-20 17:05:42 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,27 +9,27 @@ dotenv () {
 
 gopath () {
   echo "Verifying GOPATH..." &&
-    [ -z $GOPATH ] && export GOPATH=$(go env GOPATH)
+    [ -z $GOPATH ] &&
+    export GOPATH=$(go env GOPATH)
 }
 
 build () {
   (cd $GOPATH/src/github.com/linterpreteur/brunchfeed &&
 
     echo "Fetching Brunch RSS feed..." &&
-    go run main.go fetch \
-      -id "$BRUNCHFEED_BRUNCH_ID" \
-      -src "$BRUNCHFEED_DATA_PATH" &&
+      go run main.go fetch \
+        -id "$BRUNCHFEED_BRUNCH_ID" \
+        -src "$BRUNCHFEED_DATA_PATH" &&
 
     echo "Building posts data..." &&
-    go run main.go build \
-      -src "$BRUNCHFEED_DATA_PATH" \
-      -dest "$BRUNCHFEED_POST_PATH" \
-      -template "$BRUNCHFEED_TEMPLATE" &&
+      go run main.go build \
+        -src "$BRUNCHFEED_DATA_PATH" \
+        -dest "$BRUNCHFEED_POST_PATH" \
+        -template "$BRUNCHFEED_TEMPLATE" &&
 
     echo "Generating pages..." &&
       eval "$BRUNCHFEED_GENERATE")
 }
 
-dotenv
-gopath
-build
+
+dotenv && gopath && build
